881387d50970015a48d2f02debf64beb7a3d637a
okubicek <kubicek.ondrej@gmail.com>
2020-03-22 21:01:07 +0100

    Playing with DataFlow

---
+++ NetCore/DataFlow/DataFlowDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace DataFlowDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var processedDelaysCollection = new BlockingCollection<int>();
+            var options = new ExecutionDataflowBlockOptions{
+                MaxDegreeOfParallelism = 4
+            };
+
+            var actionBlock = new ActionBlock<int>(async delay =>
+            {
+                if (delay == 3 || delay == 5)
+                {
+                    throw new ArgumentException($"Nope delay {delay} is not supported, not at all");
+                }
+
+                await Task.Delay(1000 * delay);
+
+                processedDelaysCollection.Add(delay);
+            }, options);
+
+            var stopWatch = Stopwatch.StartNew();
+            foreach(var delay in new int[] {8, 6, 5, 3 })
+            {
+                actionBlock.Post(delay);
+            }
+
+            actionBlock.Complete();
+            try 
+            {
+                await actionBlock.Completion;
+            }
+            catch (AggregateException errors)
+            {
+                foreach (var ex in errors.InnerExceptions)
+                    Console.WriteLine($"{ex.Message}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine($"Processing took {stopWatch.Elapsed} secs");
+            Console.WriteLine($"Processed delays : {string.Join(',', processedDelaysCollection)}");
+        }
+    }
+}
