74d5247a1ce90b44b9a365f3a36cce07634bf18a
okubicek <kubicek.ondrej@gmail.com>
2020-12-14 14:26:16 +0100

    Day 13 part 1

--- AoC/Aoc/Program.cs
+++ AoC/Aoc/Program.cs
@@ -6,7 +6,7 @@ namespace Aoc
 	{
 		static void Main(string[] args)
 		{
-			var dayToRun = 12;
+			var dayToRun = 13;
 			var solver = new PuzzleSolverFactory().GetSolver(2020, dayToRun);
 
 			SolveTask(solver, 1);
---
+++ AoC/Aoc/Year2020/Day13/Solver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.Year2020.Day13
+{
+	[Aoc(Day = Day)]
+	public class Solver : IPuzzleSolver
+	{
+		private const int Day = 13;
+
+		private List<string> _input;
+
+		private int _earliestDeparture;
+
+		private List<int> _busses;
+
+		public Solver()
+		{
+			_input = InputFileReader.GetInput($"Year2020/Inputs/Day{Day}.txt");
+			_earliestDeparture = int.Parse(_input[0]);
+			_busses = _input[1].Split(',').Where(x => x != "x").Select(x => int.Parse(x)).ToList();
+		}
+
+		public string SolveFirstTask()
+		{
+			var departureTime = _earliestDeparture + 1;
+			while(true)
+			{			
+				foreach(var bus in _busses)
+				{
+					if (departureTime % bus == 0)
+					{
+						return $"{bus * (departureTime - _earliestDeparture)}";
+					}					
+				}
+
+				departureTime++;
+			}
+		}
+
+		public string SolveSecondTask()
+		{			
+			return $"";
+		}
+	}
+}
